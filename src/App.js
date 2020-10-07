7add68aa54a177e717876b8d79d1b8f01822ad70
MCauz <michaelhcausey@gmail.com>
2020-10-07 13:22:52 -0700

    Menu Input

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar'
 import Stories from './containers/Stories'
+import { Container } from 'semantic-ui-react'
 
 function App() {
   return (
-    <div>
+    <Container>
       <NavBar />
       <Stories />
-    </div>
+    </Container>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
 
-function NavBar() {
-    return(
-        <div>
-            NavBar Test
-        </div>
-    )
+export default class NavBar extends Component {
+    state = { activeItem: 'Top' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+            return(
+                <Menu tabular>
+                    <Menu.Item
+                        name='Top'
+                        active={activeItem === 'Top'}
+                        onClick={this.handleItemClick}
+                        />
+                    <Menu.Item
+                        name='Team News'
+                        active={activeItem === 'Team News'}
+                        onClick={this.handleItemClick}
+                        />
+                    <Menu.Item
+                        name='Player News'
+                        active={activeItem === 'Player News'}
+                        onClick={this.handleItemClick}
+                        />
+                </Menu>
+            )
+    }
 } 
 
-export default NavBar
