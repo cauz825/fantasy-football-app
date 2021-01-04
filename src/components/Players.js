053df077a5915e760856159b467c6b992a1ee909
MCauz <michaelhcausey@gmail.com>
2021-01-03 20:41:14 -0800

    tweaks

--- src/components/Players.js
+++ src/components/Players.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Players() {
     return (
         <div>
-            Top Performers
+            Top Performers<br></br>
+            1)<br></br>
+            2)<br></br>
+            3)<br></br>
         </div>
     )
 }
