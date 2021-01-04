74721f652ae34c4dcd09a3313d5930dc563d0fd4
MCauz <michaelhcausey@gmail.com>
2021-01-03 20:16:43 -0800

    added standings component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Form from './components/Form'
+import Standings from './components/Standings'
 
 function App() {   
   return (
     <div>
       Top Level
       <Form />
+      <Standings />
     </div>
   )
 }
---
+++ src/components/Standings.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Standings() {
+    return(
+        <div>
+            Standings Component
+        </div>
+    )
+}
+
+export default Standings
